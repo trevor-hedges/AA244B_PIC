c93065fcfcbe808839220bbb59a211dcc963a1ed
Trevor Hedges <thedges200@gmail.com>
2019-06-08 17:02:20 -0700

    Changed normalization. Seems to work better now for tracking energy!

--- pic.py
+++ pic.py
@@ -14,6 +14,7 @@ import quiet_start
 # Define flags
 verbose = 1
 bc_type = 1 # 0 = fixed potential, 1 = periodic, 2 = periodic + fourier
+ic_override = 1 # Whether to ignore specifying plasma properties and load pre-set initial particle conditions
 
 # Define physical constants
 q_phys = 1.60218E-19 # C
@@ -37,7 +38,7 @@ q_to_m_i = q_phys/m_i_phys
 q_to_m_e = q_phys/m_e_phys
 
 # Define simulation constants
-mi_me_ratio = 10
+mi_me_ratio = 1
 m_e_sim = m_e_phys
 m_i_sim = mi_me_ratio*m_e_phys # Mass of simulated ion
 
@@ -50,7 +51,7 @@ print('simulated macroparticle mi=' + str(macroparticle_num*m_i_sim) + " kg")
 # Calculate electron Debye length
 Ti = 50;
 Te = 50;
-lambda_d_phys = np.sqrt(eps0*K*Te/(n0*q_phys**2))
+lambda_d_phys = np.sqrt(eps0*K*Te/(n0*q_phys**2)) # meters
 print("lambda_d=" + str(lambda_d_phys) + " m")
 
 # Calculate electron plasma frequency
@@ -62,12 +63,15 @@ N_d = n0*4/3*np.pi*lambda_d_phys**3
 print("N_d=" + str(N_d))
 
 # Define sim params (make these inputs later)
-delta_x = lambda_d_phys/20
-delta_t = 0.001/omega_p_phys
+delta_x = lambda_d_phys/20 # meters
+delta_t = 0.001/omega_p_phys # s^-1
 print('delta_x=' + str(delta_x) + ' m')
 print('delta_t=' + str(delta_t) + ' s')
 
 n0_sim = n0/macroparticle_num # m^-1
+
+# Nondimensional plasma density (per debye cube)
+n0_N = n0*lambda_d_phys**3
 
 lambda_d_sim = np.sqrt(eps0*K*Te*macroparticle_num/(n0_sim*(macroparticle_num*q_phys)**2))
 print("lambda_d_sim = " + str(lambda_d_sim))
@@ -81,7 +85,7 @@ print("Normalized ion thermal velocity = " + str(vthi_sim))
 print("Normalized electron thermal velocity = " + str(vthe_sim))
 
 n_x = 11
-n_tsteps = 100
+n_tsteps = 1000
 t_tot = delta_t*n_tsteps
 plasma_cycles = (omega_p_phys/(2*np.pi))*t_tot
 print("Total time to be simulated: " + str(t_tot) + " seconds; " + str(plasma_cycles) + " plasma oscillations")
@@ -116,14 +120,16 @@ else:
 
 
 
-def interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N):
+def interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N, lambda_d, delta_x_N, n0_N):
     """Given charge locations, solves for charge density on 1D grid
     Using first-order interpolation
     """
     
+    x_i_NN = x_i_N/delta_x_N
+    x_e_NN = x_e_N/delta_x_N
     
-    ind_x_i_N = np.array(np.floor(x_i_N), dtype=int)
-    ind_x_e_N = np.array(np.floor(x_e_N), dtype=int)
+    ind_x_i_N = np.array(np.floor(x_i_NN), dtype=int)
+    ind_x_e_N = np.array(np.floor(x_e_NN), dtype=int)
 
     #ind_x_i_NP1 = (ind_x_i_N+1) % n_x
     #ind_x_e_NP1 = (ind_x_e_N+1) % n_x
@@ -131,10 +137,10 @@ def interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N):
     # print('ind_x_i: ' + str(ind_x_i_N))
     # print('ind_x_e: ' + str(ind_x_e_N))
 #    
-    chg_j_i_N = mnum_i_N*((ind_x_i_N+1)-x_i_N)
-    chg_jp1_i_N = mnum_i_N*(x_i_N-ind_x_i_N)
-    chg_j_e_N = -mnum_e_N*((ind_x_e_N+1)-x_e_N)
-    chg_jp1_e_N = -mnum_e_N*(x_e_N-ind_x_e_N)
+    chg_j_i_N = mnum_i_N*((ind_x_i_N+1)-x_i_NN)
+    chg_jp1_i_N = mnum_i_N*(x_i_NN-ind_x_i_N)
+    chg_j_e_N = -mnum_e_N*((ind_x_e_N+1)-x_e_NN)
+    chg_jp1_e_N = -mnum_e_N*(x_e_NN-ind_x_e_N)
 
 #    chg_j_i_N = 1*((ind_x_i_N+1)-x_i_N)
 #    chg_jp1_i_N = 1*(x_i_N-ind_x_i_N)
@@ -149,7 +155,10 @@ def interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N):
     np.add.at(chg_N, ind_x_e_N, chg_j_e_N)
     np.add.at(chg_N, (ind_x_e_N+1) % n_x, chg_jp1_e_N)
     
-    return(chg_N)
+    # Scale as necessary
+    rho_N = chg_N*lambda_d**2/(delta_x_N*n0_N)
+    
+    return(rho_N)
     
 def solve_potential(n_x, rho_N, V_L_N, V_R_N):
     """Given charge densities, solves for the potential on 1D grid"""
@@ -171,7 +180,7 @@ def solve_potential(n_x, rho_N, V_L_N, V_R_N):
     
     return(phi_N)
     
-def solve_potential_periodic(n_x, rho_N, G_N):
+def solve_potential_periodic(n_x, rho_N, G_N, delta_x_N):
     """Given charge densities, solves for potential on 1D periodic grid"""
     
     # Construct A matrix
@@ -180,6 +189,10 @@ def solve_potential_periodic(n_x, rho_N, G_N):
 #    A[0, -1] = 1
     A = -A
     b = rho_N[1:]
+    
+    # Scale
+    b = b*delta_x_N**2
+    
     b[0] += G_N
     b[-1] += G_N
     
@@ -234,61 +247,74 @@ def solve_efield_from_potential(n_x, phi_N, V_L_N, V_R_N):
     # Return E-field    
     return(E_N)
 
-def solve_efield_from_potential_periodic(phi_N):
+def solve_efield_from_potential_periodic(phi_N, delta_x_N):
     """Given potential field, solves for e-field in 1D"""
     
     # Perform calculation
-    E_N = -(np.roll(phi_N, -1) - np.roll(phi_N, 1))/2
+    E_N = -(np.roll(phi_N, -1) - np.roll(phi_N, 1))/(2*delta_x_N)
     
     # Return E-field    
     return(E_N)
 
 
-def integrate_motion(n_x, delta_x, delta_t, E_N, x_i_N, x_e_N, v_i_N, v_e_N, mnum_i_N, mnum_e_N):
+def integrate_motion(n_x, E_N, x_i_N, x_e_N, v_i_N, v_e_N, delta_x_N, delta_t_N, mi_me_ratio_N):
     """Use Leapfrog method to integrate new particle velocities and positions """
     
     # Interpolate E-field to each particle
     # np.interp uses first-order interpolator, needed when combined w/ first-order charge-to-grid
-    E_interp_i_N = np.interp(x_i_N, np.arange(n_x), E_N, period=n_x)
-    E_interp_e_N = np.interp(x_e_N, np.arange(n_x), E_N, period=n_x)
+    x_i_NN = x_i_N/delta_x_N
+    x_e_NN = x_e_N/delta_x_N
+    
+    E_interp_i_N = np.interp(x_i_NN, np.arange(n_x), E_N, period=n_x)
+    E_interp_e_N = np.interp(x_e_NN, np.arange(n_x), E_N, period=n_x)
     
     # Calculate new velocities at time t+delta_t/2
-    v_i_new_N = (q_phys*delta_t)**2/(m_i_sim*eps0*delta_x)*E_interp_i_N + v_i_N
-    v_e_new_N = -(q_phys*delta_t)**2/(m_e_sim*eps0*delta_x)*E_interp_e_N + v_e_N
+    v_i_new_N = 1/mi_me_ratio_N*E_interp_i_N*delta_t_N+v_i_N
+    v_e_new_N = -E_interp_e_N*delta_t_N+v_e_N
+
+#    v_i_new_N = (q_phys*delta_t)**2/(m_i_sim*eps0*delta_x)*E_interp_i_N + v_i_N
+#    v_e_new_N = -(q_phys*delta_t)**2/(m_e_sim*eps0*delta_x)*E_interp_e_N + v_e_N
     
     # Calculate new positions at time t+delta_t with modulos to handle periodic boundaries
-    x_i_new_N = (x_i_N + v_i_new_N) % (n_x)
-    x_e_new_N = (x_e_N + v_e_new_N) % (n_x)
+    x_i_new_N = (x_i_N + v_i_new_N*delta_t_N) % (n_x*delta_x_N)
+    x_e_new_N = (x_e_N + v_e_new_N*delta_t_N) % (n_x*delta_x_N)
         
     # Return new values
     return(x_i_new_N, x_e_new_N, v_i_new_N, v_e_new_N)
         
-def calculate_energy(x_i_old_N, x_e_old_N, v_i_old_N, v_e_old_N, v_i_new_N, v_e_new_N, mnum_i_N, mnum_e_N, chg_N, phi_N):
+def calculate_energy(v_i_old_N, v_e_old_N, v_i_new_N,
+                     v_e_new_N, mnum_i_N, mnum_e_N, rho_N, phi_N, delta_x,
+                     omega_p_phys, lambda_d_phys, n0, m_e_phys, mi_me_ratio):
     """Calculate energy in system at time t. INPUTS: x_i(t), x_e(t),
     v_i(t-delta_t/2), v_e(t-delta_t/2), v_i(t+delta_t/2), v_e(t+delta_t/2)
     macroparticle number (ions), macroparticle number (electrons),
     charge density, electric potential """
     
     # Calculate velocity squared at time t (approximate to O(delta_t^2))
-    v_i_t_squared = v_i_old_N*v_i_new_N
-    v_e_t_squared = v_e_old_N*v_e_new_N
+    v_i_t_squared_N = v_i_old_N*v_i_new_N
+    v_e_t_squared_N = v_e_old_N*v_e_new_N
     
-    # Calculate kinetic energy for each species
-    KE_i_N = 1/2*mi_me_ratio*np.inner(mnum_i_N, v_i_t_squared)
-    KE_e_N = 1/2*np.inner(mnum_e_N, v_e_t_squared)
+    # Calculate dimensional kinetic energy for each species    
+    KE_i = 1/2*m_e_phys*mi_me_ratio*lambda_d_phys**2*omega_p_phys**2*np.inner(mnum_i_N, v_i_t_squared_N)
+    KE_e = 1/2*m_e_phys*lambda_d_phys**2*omega_p_phys**2*np.inner(mnum_e_N, v_e_t_squared_N)
     
-    # Calculate electric potential energy
-    PE_N = np.inner(chg_N, phi_N)
+    #np.inner(mnum_i_N, v_i_t_squared)
+    #KE_e_N = 1/2*np.inner(mnum_e_N, v_e_t_squared)
+    
+    # Calculate dimensional electric potential energy
+    PE = delta_x*omega_p_phys**2*lambda_d_phys**2*m_e_phys*n0/2*np.inner(rho_N, phi_N)
+    
+    #PE_N = 1/2*q_phys**2/eps0*np.inner(chg_N, phi_N)
     
     # Calculate total energy 
-    E_tot_N = KE_i_N + KE_e_N + PE_N
+    E_tot = KE_i + KE_e + PE
     
     # Print stuff
-#    print("Ion kinetic: " + str(KE_i_N))
-#    print("Electron kinetic: " + str(KE_e_N))
-#    print("Electric potential: " + str(PE_N))
+    print("Ion kinetic: " + str(KE_i))
+    print("Electron kinetic: " + str(KE_e))
+    print("Electric potential: " + str(PE))
     
-    return(E_tot_N)
+    return(E_tot, KE_i, KE_e, PE)
 
 def phase_plot(x_i_N, x_e_N, v_i_N, v_e_N, tstep, axes):
 
@@ -311,50 +337,100 @@ def all_plot(NP, n_x, x_i_N, x_e_N, E_N, phi_N, tstep, axes):
     plt.plot(xrp1, np.concatenate((phi_N/10, np.array([phi_N[0]/10]))), color='red', label='phi')
     plt.plot(xrp1, np.concatenate((E_N, np.array([E_N[0]]))), color='blue', label='E')
     plt.xlim(axes[0:2])
-    plt.ylim(axes[2:4])
+#    plt.ylim(axes[2:4])
     plt.legend()
     plt.savefig('img/all_debug/all_' + str(tstep) + '.png')
+    plt.close()
+    
+def energy_plot(n_tsteps, Etot_vs_t, KEi_vs_t, KEe_vs_t, PE_vs_t):
+    
+    plt.figure()
+    plt.plot(np.arange(n_tsteps), Etot_vs_t, label='Total energy')
+    plt.plot(np.arange(n_tsteps), KEi_vs_t, label='Ion KE')
+    plt.plot(np.arange(n_tsteps), KEe_vs_t, label='Electron KE')
+    plt.plot(np.arange(n_tsteps), KEi_vs_t+KEe_vs_t, label='Total KE')
+    plt.plot(np.arange(n_tsteps), PE_vs_t, label='Electric PE')
+    plt.title("Total energy vs time")
+    plt.xlabel("Timestep")
+    plt.ylabel("Energy (J/m^2)")
+    plt.legend()
+    plt.savefig("img/energy/energy_vs_time.png")
     plt.close()
 
 # Tests...
 if safe_to_run:
 
-    # Initialize particle positions TODO: add some randomization (?)
-    margin = 0
-#    x_i_N = np.arange(margin,n_x-margin,n_x/NP)
-#    x_e_N = np.arange(n_x/(2*NP)+margin,n_x-margin,n_x/NP)
-    jumble_pos = 1
-    if jumble_pos:
-        jumble_factor = 0.5
-        x_i_N = (np.linspace(margin,n_x-margin,NP) + jumble_factor*np.random.normal(size=NP)) % n_x
-        x_e_N = (np.linspace(margin,n_x-margin,NP) + jumble_factor*np.random.normal(size=NP)) % n_x
+    # Nondimensionalize constants
+    delta_x_N = delta_x/lambda_d_phys
+    delta_t_N = delta_t*omega_p_phys
+    
+    if ic_override:
+        
+        delta_x = 1
+        delta_t = 3E-6
+        
+        # Nondimensionalize constants
+        delta_x_N = delta_x/lambda_d_phys
+        delta_t_N = delta_t*omega_p_phys
+        
+        NP = 8
+        x_i_NN = np.array([4.5, 4.9, 5.0, 5.1, 5.5, 6, 6.5, 8])
+        x_e_NN = np.array([1, 1.4, 1.6, 2.6, 3.5, 4.7, 7, 9])
+        #v_i_NN = np.array([0,0,0,0,0,0,0,0])
+        #v_e_NN = np.array([0,0,0,0,0,0,0,0])
+        
+        x_i_N = x_i_NN*delta_x_N
+        x_e_N = x_e_NN*delta_x_N
+        v_i_N = np.zeros(NP)
+        v_e_N = np.zeros(NP)
+        
     else:
-        x_i_N = np.linspace(margin,n_x-margin,NP)
-        x_e_N = np.linspace(n_x/(2*NP)+margin,n_x-margin,NP)
-    
-    if verbose:
-        print("Initial ion locations: " + str(x_i_N))
-        print("Initial electron locations: " + str(x_e_N))
-    
+        # Initialize particle positions TODO: add some randomization (?)
+        margin = 0
+    #    x_i_N = np.arange(margin,n_x-margin,n_x/NP)
+    #    x_e_N = np.arange(n_x/(2*NP)+margin,n_x-margin,n_x/NP)
+        jumble_pos = 1
+        if jumble_pos:
+            jumble_factor = 0.5
+            x_i_N = (np.linspace(margin,n_x-margin,NP) + jumble_factor*np.random.normal(size=NP)) % n_x
+            x_e_N = (np.linspace(margin,n_x-margin,NP) + jumble_factor*np.random.normal(size=NP)) % n_x
+        else:
+            x_i_N = np.linspace(margin,n_x-margin,NP)
+            x_e_N = np.linspace(n_x/(2*NP)+margin,n_x-margin,NP)
+        
+        if verbose:
+            print("Initial ion locations: " + str(x_i_N))
+            print("Initial electron locations: " + str(x_e_N))
+            
+        # Initialize particle velocities using quiet start technique    
+        v_i_N = quiet_start.maxwellian(NP, 0, vthe_sim)
+        v_e_N = quiet_start.maxwellian(NP, vthe_sim*0, vthe_sim)    
+        if verbose:
+            print("Initial ion normalized velocities: " + str(v_i_N))
+            print("Initial electron normalized velocities: " + str(v_e_N))
+
+    # Allocate total energy array
+    Etot_vs_t = np.zeros(n_tsteps)
+    KEi_vs_t = np.zeros(n_tsteps)
+    KEe_vs_t = np.zeros(n_tsteps)
+    PE_vs_t = np.zeros(n_tsteps)
+
     # Macroparticle number arrays
     mnum_i_N = macroparticle_num*np.ones(np.size(x_i_N))
     mnum_e_N = macroparticle_num*np.ones(np.size(x_e_N))
-    
-    # Initialize particle velocities using quiet start technique    
-    v_i_N = quiet_start.maxwellian(NP, 0, vthe_sim)
-    v_e_N = quiet_start.maxwellian(NP, vthe_sim*0, vthe_sim)    
-    if verbose:
-        print("Initial ion normalized velocities: " + str(v_i_N))
-        print("Initial electron normalized velocities: " + str(v_e_N))
 
     if bc_type == 0:
         # Nondimensionalize boundary conditions
         V_L_N = eps0/(q_phys*delta_x) * V_L
         V_R_N = eps0/(q_phys*delta_x) * V_R
     elif bc_type == 1:
-        G_N = eps0/(q_phys*delta_x) * G
+        G_N = q_phys/(omega_p_phys**2*m_e_phys*lambda_d_phys**2) * G
 
-        
+
+    # Nondimensionalize constants
+    #delta_x_N = delta_x/lambda_d_phys
+    #delta_t_N = delta_t*omega_p_phys
+
     # Allocate time history of particle trajectories
     #x_i_N_T = np.zeros([n_tsteps, np.size(x_i_N)])
     #x_e_N_T = np.zeros([n_tsteps, np.size(x_i_N)])
@@ -362,24 +438,28 @@ if safe_to_run:
     # Main Loop        
     for t_N in np.arange(n_tsteps):
         print('evaluating timestep ' + str(t_N))
-        chg_N = interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N)
+        # (n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N, lambda_d, delta_x_N, n0_N):
+        rho_N = interpolate_charges(n_x, x_i_N, x_e_N, mnum_i_N, mnum_e_N, lambda_d_phys, delta_x_N, n0_N)
         
         if bc_type==2:
-            phi_N, E_N = solve_potential_fourier(n_x, chg_N)
+            phi_N, E_N = solve_potential_fourier(n_x, rho_N)
         elif bc_type==1:
-            phi_N = solve_potential_periodic(n_x, chg_N, G_N)
-            E_N = solve_efield_from_potential_periodic(phi_N)
+            phi_N = solve_potential_periodic(n_x, rho_N, G_N, delta_x_N)
+            E_N = solve_efield_from_potential_periodic(phi_N, delta_x_N)
         else:
-            phi_N = solve_potential(n_x, chg_N, V_L_N, V_R_N)
+            phi_N = solve_potential(n_x, rho_N, V_L_N, V_R_N)
             E_N = solve_efield_from_potential(n_x, phi_N, V_L_N, V_R_N)
             
-        x_i_new_N, x_e_new_N, v_i_new_N, v_e_new_N = integrate_motion(n_x, delta_x, delta_t, E_N, x_i_N, x_e_N, v_i_N, v_e_N, mnum_i_N, mnum_e_N)
+        x_i_new_N, x_e_new_N, v_i_new_N, v_e_new_N = integrate_motion(n_x, E_N, x_i_N, x_e_N, v_i_N, v_e_N, delta_x_N, delta_t_N, mi_me_ratio)
         
         # Calculate total energy
-        E_old = calculate_energy(x_i_N, x_e_N, v_i_N, v_e_N, v_i_new_N, v_e_new_N, mnum_i_N, mnum_e_N, chg_N, phi_N)
+        #E_old = calculate_energy(x_i_N, x_e_N, v_i_N, v_e_N, v_i_new_N, v_e_new_N, mnum_i_N, mnum_e_N, chg_N, phi_N)
+        Etot_vs_t[t_N], KEi_vs_t[t_N], KEe_vs_t[t_N], PE_vs_t[t_N] = calculate_energy(v_i_N, v_e_N, v_i_new_N,
+                     v_e_new_N, mnum_i_N, mnum_e_N, rho_N, phi_N, delta_x,
+                     omega_p_phys, lambda_d_phys, n0, m_e_phys, mi_me_ratio)
         
         # Print to console
-        print("Total system energy = " + str(E_old))
+        print("Total system energy = " + str(Etot_vs_t[t_N]) + " J/m^2")
         
         # Save particle positions
         #x_i_N_T[t_N,:] = x_i_N
@@ -398,15 +478,19 @@ if safe_to_run:
         
         # Plot normalized charge density
         plt.figure()
-        plt.plot(np.arange(0,n_x,1), chg_N)
-        plt.savefig("img/density/n_N" + str(t_N) + ".png")
+        plt.plot(np.arange(0,n_x,1), rho_N)
+        plt.savefig("img/density/rho_N" + str(t_N) + ".png")
         plt.close()
         
         # Plot particle positions
-        phase_plot(x_i_new_N, x_e_N, v_i_N, v_e_N, t_N, [0, n_x, -0.3, 0.3])
+        x_i_NN = x_i_N/delta_x_N
+        x_e_NN = x_e_N/delta_x_N
+        v_i_NN = v_i_N*delta_t_N/delta_x_N
+        v_e_NN = v_e_N*delta_t_N/delta_x_N
+        phase_plot(x_i_NN, x_e_NN, v_i_NN, v_e_NN, t_N, [0, n_x, -0.3, 0.3])
     
-        # Make plots of particles + fields superimposed
-        all_plot(NP, n_x, x_i_new_N, x_e_new_N, E_N, phi_N, t_N, [0, n_x, -50000, 50000])
+        # Make plots of particles + fields superimposed at time t=0
+        all_plot(NP, n_x, x_i_NN, x_e_NN, E_N, phi_N, t_N, [0, n_x, -50000, 50000])
     
         # Set new
         x_i_N = x_i_new_N
@@ -415,6 +499,8 @@ if safe_to_run:
         v_e_N = v_e_new_N
         
     #t = np.arange(n_tsteps)*delta_t
+    # Save plot of total energy
+    energy_plot(n_tsteps, Etot_vs_t, KEi_vs_t, KEe_vs_t, PE_vs_t)
 
 
 
